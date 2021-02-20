209a77ead2aeaa26801a108e379948a972468186
Daan1241 <45517731+Daan1241@users.noreply.github.com>
2021-02-20 23:10:01 +0100

    init files

--- dependencies/php/pdo.php
+++ dependencies/php/pdo.php
@@ -1,7 +1,7 @@
 <?php
 $dbservername = "localhost";
-$dbusername = "forevertools";
-$dbpassword = "forevertools_password"; // Sensitive data, do not commit to Github
+$dbusername = "XXXXXX";
+$dbpassword = "XXXXXX"; // Sensitive data, do not commit to Github
 $connection = new PDO("mysql:host=$dbservername;dbname=toolsforever", $dbusername, $dbpassword);
 
 ?>
