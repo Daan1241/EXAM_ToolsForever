8ec54a5938c3c607af80392ef2f9342c1bd57c10
Daan1241 <45517731+Daan1241@users.noreply.github.com>
2021-04-12 15:20:34 +0200

    Added a page to order products. Products low or out of stock now have their own section where new stock can quickly be ordered. + some bug fixes

--- commitAndPush.cmd
+++ commitAndPush.cmd
@@ -1,5 +1,5 @@
 git add *
 git reset dependencies/php/pdo.php
-git commit -m "Only administrators can create accounts now, removed product ID's, removed location descriptions, products can now be copied from locaiton A to location B."
+git commit -m "Added a page to order products. Products low or out of stock now have their own section where new stock can quickly be ordered. + some bug fixes"
 git push -f
 pause
